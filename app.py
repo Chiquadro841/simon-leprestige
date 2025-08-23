9d94c30144db6ab926b8175d69c81e26b7191caf
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-08-23 22:58:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -132,9 +132,9 @@ col_left, col_center, col_right = st.columns([1,3,1])
 media_dir = Path.cwd() / "images"
 
 media = [
-    "jose_bobadilla.jpg",
-    "yamil_raidan.jpg",
-    "magician_silvan.jpg",
+    #"jose_bobadilla.jpg",
+    #"yamil_raidan.jpg",
+    #"magician_silvan.jpg",
     "patrick_wave.jpg",
     #"orietta.jpg",
     #"porsche.jpg",
@@ -177,6 +177,7 @@ st.markdown("""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
