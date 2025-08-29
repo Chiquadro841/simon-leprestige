23c57d8b3d3f423fedb0862cc7f1a511485b6ca4
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-08-29 16:42:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,9 @@ with col_center:
                     if file.lower().endswith((".jpg", ".jpeg", ".png")):
                         st.image(file_path, use_container_width=True)
                     elif file.lower().endswith((".mp4", ".mov", ".webm")):
-                        st.video(file, start_time=0)
+                        with open(file, "rb") as f:
+                            video_bytes = f.read()
+                        st.video(video_bytes, start_time=0)
 
 
 
@@ -201,6 +203,7 @@ st.markdown("""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
