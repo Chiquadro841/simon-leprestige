f94235e39d30d8ad65d01f05b63f3e7d7ebd8fca
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-08-29 17:17:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ with col3:
     st.markdown("""
 # Che tipo di eventi esclusivi? 
 Si esibisce esclusivamente in ambienti privati e di altissimo livello, tra cui ville, super yacht di prestigio, location boutique e cerimonie riservate.\\
-Ogni sua performance è pensata per offrire un’esperienza unica, irripetibile e altamente personalizzata, in cui il pubblico viene immerso in un’atmosfera di meraviglia e stupore.\\
+Ogni sua performance è pensata per offrire un’esperienza unica, irripetibile e altamente personalizzata, in cui il pubblico viene immerso in un’atmosfera di meraviglia e stupore.
 
 La sua arte non è mai banale: accetta solo 20 performance all’anno, garantendo così che ogni spettacolo mantenga un livello di qualità impeccabile e un carattere assolutamente esclusivo.\\
 Dalla scelta dei dettagli scenici all’interazione con gli ospiti, 
@@ -203,6 +203,7 @@ st.markdown("""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
