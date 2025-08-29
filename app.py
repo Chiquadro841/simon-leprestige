3fe40bde90d2e9663f8e93829eeef53758d22221
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-08-29 14:32:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ with col3:
     st.markdown("""
     <div style="display: flex; justify-content: flex-end; gap: 40px; font-size: 18px; align-items: center;">
         <div>Email: <a href="mailto:tuoindirizzo@email.com">tuoindirizzo@email.com</a></div>
-        <div><p><strong>Telefono:</strong></p><p>+39 333 1234567</p></div>
+        <div><p>Telefono:</p><p>+39 333 1234567</p></div>
         <div>Instagram: <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></div>
     </div>
     """, unsafe_allow_html=True)
@@ -197,6 +197,7 @@ st.markdown("""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
