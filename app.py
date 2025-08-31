72df419d67028460cbc7b1300080cb0743845ff3
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-08-31 16:54:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,10 @@ logo = Image.open("images/logo4.png")  # meglio PNG trasparente
 # --- Layout a 3 colonne: sinistra vuota, centro logo, destra contatti ---
 col1, col2, col3 = st.columns([1.6, 1.8, 1])  # proporzioni: logo stretto, contatti più larghi
 
-with col2:
+with col1:
     st.empty()  # colonna sinistra vuota
 
-with col1:
+with col2:
     st.image(logo, width=200)
 
 with col3:
@@ -204,6 +204,7 @@ st.markdown("""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
