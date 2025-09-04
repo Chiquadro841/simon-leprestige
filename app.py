85f6c4f78b61a94ba97731d3410447296e27eb2a
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-09-05 00:48:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-cell = "Telefono"
+
 
 # --- Layout a 3 colonne: sinistra vuota, centro logo, destra contatti ---
 col1, col2, col3 = st.columns([1, 1, 1])  # tutte larghe uguali
@@ -100,7 +100,7 @@ with col2:
     )
 
 with col3:
-    
+    cell= st.empty()
     st.markdown(f"""
     <div style="font-size:16px; text-align:center;">
         <div>Email: <a href="mailto:zmorossi@gmail.com">zmorossi@gmail.com</a></div>
@@ -278,6 +278,7 @@ st.markdown(f"""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
