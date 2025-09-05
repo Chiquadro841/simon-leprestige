b368910bfd3ce674981bd0e3dbfb05bd791b71f2
Brandon96 <152106838+Chiquadro841@users.noreply.github.com>
2025-09-05 15:45:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -292,9 +292,8 @@ with c2:
     img = load_and_crop(immagini_path[st.session_state.index])
     st.image(img, width=400, caption=f"{nome_foto}    {st.session_state.index+1} di {len(immagini_path)}")
 
-    st.markdown('<div style="text-align:right;">', unsafe_allow_html=True)
-    st.button("▶", on_click=avanti)
-    st.markdown('</div>', unsafe_allow_html=True)
+st.write("&nbsp;"*50, unsafe_allow_html=True)  # sposta a destra
+st.button("▶", on_click=avanti)
 
 
 
@@ -312,6 +311,7 @@ st.markdown(f"""
 <p><strong>Instagram:</strong> <a href="https://www.instagram.com/simone98rossi" target="_blank">@simone98rossi</a></p>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
